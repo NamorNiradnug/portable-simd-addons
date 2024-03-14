7114258fbdbcec8ff87b26dd381d30544bd53dae
NamorNiradnug <roma57linux@gmail.com>
2024-03-15 02:04:06 +0300

    asin, acos

--- src/math/trig.rs
+++ src/math/trig.rs
@@ -1,5 +1,5 @@
 use std::{
-    f32::consts::FRAC_2_PI,
+    f32::consts::{FRAC_2_PI, FRAC_PI_2},
     simd::{
         cmp::{SimdPartialEq, SimdPartialOrd},
         num::SimdFloat,
@@ -7,7 +7,7 @@ use std::{
     },
 };
 
-use crate::{math::util::sign_mask_f32, polynomial_simd};
+use crate::{math::util::FloatBitUtils, polynomial_simd};
 
 pub trait Trigonometry {
     /// Calculates sine of each lane. Overflows on large values (greater than 10⁵) and return `0.0`
@@ -63,7 +63,7 @@ pub trait Trigonometry {
     fn asin(self) -> Self;
     fn acos(self) -> Self;
     fn atan(self) -> Self;
-    fn atan2(self, other: Self) -> Self;
+    fn atan2(self, x: Self) -> Self;
 }
 
 /// π/4 reduction of `x`. Large values (greater than 10⁵) are treated as zeros.
@@ -151,7 +151,9 @@ where
 
         let sin_cos_swap = (quadrants & Simd::splat(1)).simd_eq(Simd::default());
         let sin_vals = sin_cos_swap.select(sin, cos);
-        Simd::from_bits(sin_vals.to_bits() ^ sin_sign_adj(quadrants) ^ sign_mask_f32(self))
+        sin_vals
+            .sign_combine(self)
+            .sign_combine(Simd::from_bits(sin_sign_adj(quadrants)))
     }
 
     #[inline]
@@ -161,15 +163,36 @@ where
 
         let sin_cos_swap = (quadrants & Simd::splat(1)).simd_eq(Simd::default());
         let cos_vals = sin_cos_swap.select(cos, sin);
-        Simd::from_bits(cos_vals.to_bits() ^ cos_sign_adj(quadrants))
+        cos_vals.sign_combine(Simd::from_bits(cos_sign_adj(quadrants)))
     }
 
+    #[inline]
     fn asin(self) -> Self {
-        todo!()
+        const P0: f32 = 1.666_675_2E-1;
+        const P1: f32 = 7.495_300_3E-2;
+        const P2: f32 = 4.547_002_6E-2;
+        const P3: f32 = 2.418_131E-2;
+        const P4: f32 = 4.216_32E-2;
+
+        let x_abs = self.abs();
+        let big = x_abs.simd_ge(Simd::splat(0.5));
+
+        // for x >= 0: π/2 - 2 arcsin √((1-x)/2) = arcsin x
+        // taylor_arg is less than or equal to 0.5
+        let taylor_arg2 = big.select((Simd::splat(1.0) - x_abs) * Simd::splat(0.5), x_abs * x_abs);
+        let taylor_arg = big.select(taylor_arg2.sqrt(), x_abs);
+        let taylor_result = polynomial_simd!(taylor_arg2; P0, P1, P2, P3, P4)
+            .mul_add(taylor_arg * taylor_arg2, taylor_arg);
+
+        let asin_abs = big.select(
+            Simd::splat(FRAC_PI_2) - (taylor_result + taylor_result),
+            taylor_result,
+        );
+        asin_abs.sign_combine(self)
     }
 
     fn acos(self) -> Self {
-        todo!()
+        Simd::splat(FRAC_PI_2) - self.asin()
     }
 
     fn atan(self) -> Self {
@@ -209,14 +232,12 @@ mod test {
     }
 
     #[bench]
-    #[no_mangle]
     fn scalar_sin_bench(b: &mut test::Bencher) {
         let data = linspace(-1e4..1e4, BENCH_POINTS);
         b.iter(|| data.iter().map(|x| x.sin()).sum::<f32>())
     }
 
     #[bench]
-    #[no_mangle]
     fn vec_sin_cos_bench(b: &mut test::Bencher) {
         let data = linspace(-1e4..1e4, BENCH_POINTS);
         b.iter(|| {
@@ -231,7 +252,7 @@ mod test {
 
     #[test]
     fn sin_accurancy() {
-        let data = linspace(-1e4..1e4, 100_000);
+        let data = linspace(-1e4..1e4, 1_000_000);
         for x in data {
             assert_abs_diff_eq!(x.sin(), f32x1::splat(x).sin()[0], epsilon = 10e-6);
         }
@@ -239,9 +260,20 @@ mod test {
 
     #[test]
     fn cos_accurancy() {
-        let data = linspace(-1e4..1e4, 100_000);
+        let data = linspace(-1e4..1e4, 1_000_000);
         for x in data {
             assert_abs_diff_eq!(x.cos(), f32x1::splat(x).cos()[0], epsilon = 10e-6);
+        }
+    }
+
+    #[test]
+    fn asin_acos_accurancy() {
+        let data = linspace(-1.0..1.0, 100_000);
+        for x in data {
+            let vec = f32x1::splat(x);
+            assert_ulps_eq!(x.asin(), vec.asin()[0]);
+            assert_ulps_eq!(x.acos(), vec.acos()[0]);
+            assert_ulps_eq!(x, vec.asin().sin()[0]);
         }
     }
 }
--- src/math/util.rs
+++ src/math/util.rs
@@ -1,41 +1,64 @@
-use std::simd::{num::SimdFloat, LaneCount, Simd, SupportedLaneCount};
+use std::{
+    ops::BitXor,
+    simd::{num::SimdFloat, LaneCount, Simd, SupportedLaneCount},
+};
 
-/// Returns `1u32 << 31` if sign-bit is set and zero otherwise.
-///
-/// # Examples
-/// ```
-/// # #![feature(portable_simd)]
-/// # use std::simd::prelude::*;
-/// # use simd_addons::math::util::*;
-/// assert_eq!(
-///     sign_mask_f32(f32x4::from_array([0.0, -0.0, 1.0, -f32::INFINITY])),
-///     u32x4::from_array([0, 1, 0, 1]) << 31
-/// )
-/// ```
-#[inline]
-pub fn sign_mask_f32<const N: usize>(value: Simd<f32, N>) -> Simd<u32, N>
-where
-    LaneCount<N>: SupportedLaneCount,
-{
-    value.to_bits() & Simd::splat(1 << 31)
+pub trait FloatBitUtils: SimdFloat {
+    /// Returns zero if sign-bit of the value is zero and sign bit mask otherwise.
+    ///
+    /// # Examples
+    /// ```
+    /// # #![feature(portable_simd)]
+    /// # use std::simd::prelude::*;
+    /// # use simd_addons::math::util::*;
+    /// assert_eq!(
+    ///     f32x4::from_array([0.0, -0.0, 1.0, -f32::INFINITY]).sign_mask(),
+    ///     u32x4::from_array([0, 1, 0, 1]) << 31
+    /// );
+    /// assert_eq!(
+    ///     f64x4::from_array([0.0, -0.0, 1.0, -f64::INFINITY]).sign_mask(),
+    ///     u64x4::from_array([0, 1, 0, 1]) << 63
+    /// );
+    /// ```
+    fn sign_mask(self) -> Self::Bits;
+
+    /// Returns each element with the magnitude `self` and sign combined from signs of `self` and `other`.
+    /// Does roughly the same as `self * other.signum()`.
+    ///
+    /// # Examples
+    /// ```
+    /// # #![feature(portable_simd)]
+    /// # use std::simd::prelude::*;
+    /// # use simd_addons::math::util::*;
+    /// let x = Simd::from_array([1.0, 0.0, -0.0, -2.0]);
+    /// let signs = Simd::from_array([1.0, -2.0, -f32::INFINITY, -1.0]);
+    /// assert_eq!(
+    ///     x.sign_combine(signs),
+    ///     Simd::from_array([1.0, -0.0, 0.0, 2.0])
+    /// )
+    /// ```
+    fn sign_combine(self, other: Self) -> Self
+    where
+        Self::Bits: BitXor<Output = Self::Bits>,
+    {
+        Self::from_bits(self.to_bits() ^ other.sign_mask())
+    }
 }
 
-/// Returns `1u64 << 63` if sign-bit is set and zero otherwise.
-///
-/// # Examples
-/// ```
-/// # #![feature(portable_simd)]
-/// # use std::simd::prelude::*;
-/// # use simd_addons::math::util::*;
-/// assert_eq!(
-///     sign_mask_f64(f64x4::from_array([0.0, -0.0, 1.0, f64::NEG_INFINITY])),
-///     u64x4::from_array([0, 1, 0, 1]) << 63
-/// )
-/// ```
-#[inline]
-pub fn sign_mask_f64<const N: usize>(value: Simd<f64, N>) -> Simd<u64, N>
+impl<const N: usize> FloatBitUtils for Simd<f32, N>
 where
     LaneCount<N>: SupportedLaneCount,
 {
-    value.to_bits() & Simd::splat(1 << 63)
+    fn sign_mask(self) -> Simd<u32, N> {
+        self.to_bits() & Simd::splat(1 << 31)
+    }
+}
+
+impl<const N: usize> FloatBitUtils for Simd<f64, N>
+where
+    LaneCount<N>: SupportedLaneCount,
+{
+    fn sign_mask(self) -> Simd<u64, N> {
+        self.to_bits() & Simd::splat(1 << 63)
+    }
 }
